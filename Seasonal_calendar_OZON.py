7e38d14ee6cb82003e04a02c18d2a79e628e7ce5
IvanSemeikin <149505658+IvanSemeikin@users.noreply.github.com>
2023-11-17 13:05:55 +0300

    Update Seasonal_calendar_OZON.py

--- Seasonal_calendar_OZON.py
+++ Seasonal_calendar_OZON.py
@@ -15,13 +15,13 @@ df = pd.DataFrame(data)
 fig = go.Figure()
 
 # Добавляем столбцы для Продаж
-fig.add_trace(go.Bar(x=df['День'], y=df['Продажи'], name='Продажи', yaxis='y1', marker_color='purple', width=0.4)) 
+fig.add_trace(go.Bar(x=df['День'], y=df['Продажи'], name='Продажи', yaxis='y1', marker_color='pink', width=0.4)) 
 
 # Создаем вторую ось для Выручки
 fig.add_trace(go.Bar(x=df['День'], y=df['Выручка'], name='Выручка', yaxis='y2', marker_color='red', width=0.4, offset=0.2))
 
 # Создаем третью ось для Среднего чека
-fig.add_trace(go.Scatter(x=df['День'] + 0.2, y=df['Средний_чек'], mode='lines', name='Средний чек', yaxis='y3', marker_color='pink'))
+fig.add_trace(go.Scatter(x=df['День'] + 0.2, y=df['Средний_чек'], mode='lines', name='Средний чек', yaxis='y3', marker_color='purple'))
 
 # Настройка макета
 fig.update_layout(title='График продаж, выручки и среднего чека',
