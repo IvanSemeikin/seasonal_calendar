09406641b66b776a7d08d502450bb2e67e996f67
IvanSemeikin <149505658+IvanSemeikin@users.noreply.github.com>
2023-11-17 12:27:36 +0300

    Update Seasonal_calendar_OZON.py

--- Seasonal_calendar_OZON.py
+++ Seasonal_calendar_OZON.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
+import plotly.express as px
 
 # Пример данных
 data = {'День': [1, 2, 3, 4, 5],
@@ -11,34 +11,9 @@ data = {'День': [1, 2, 3, 4, 5],
 df = pd.DataFrame(data)
 
 # Строим график с тремя осями
-chart = alt.Chart(df).mark_bar().encode(
-    x='День',
-    y='Продажи',
-    color=alt.value('blue')
-).properties(
-    title='График продаж, выручки и среднего чека'
-)
-
-# Добавляем вспомогательные оси для выручки и среднего чека
-revenue_axis = alt.Axis(title='Выручка', grid=False, position='right')
-average_check_axis = alt.Axis(title='Средний чек', grid=False, position='right', offset=50)
-
-chart = chart.encode(
-    y='Выручка:Q',
-    color=alt.value('orange')
-).add_selection(
-    alt.selection_single(bind='legend', fields=['axis'], init={'axis': 'left'})
-).transform_fold(
-    ['Продажи', 'Выручка', 'Средний_чек'],
-    as_=['axis', 'value']
-).transform_calculate(
-    "axis", alt.expr.if_(alt.datum.axis == "Средний_чек", "right", "left")
-).encode(
-    y=alt.Y('value:Q', axis=alt.Axis(title='Продажи', grid=False)),
-    color=alt.Color('axis:N', legend=None)
-).properties(
-    height=300
-)
+fig = px.bar(df, x='День', y='Продажи', color='Продажи', title='График продаж', 
+             labels={'Продажи': 'Продажи', 'Выручка': 'Выручка', 'Средний_чек': 'Средний чек'},
+             secondary_y=['Выручка', 'Средний_чек'])
 
 # Отображаем график в Streamlit
-st.altair_chart(chart, use_container_width=True)
+st.plotly_chart(fig, use_container_width=True)
